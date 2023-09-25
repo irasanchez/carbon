2a9e59a912dc663fd0e178eb8b5c08d64dc36afe
ira sanchez <ira.d.sanchez@gmail.com>
2023-09-25 09:15:11 -0700

    Adds Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/low-poly-grid-haikei.svg';
+	import welcome_fallback from '$lib/images/low-poly-grid-haikei.svg';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Carbon: Gamified task management" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		I made this app, because I really liked Habitica when it was working
+		well. But, it had some missing features that I really needed like being
+		able to see things temporally and not just as a list. Additionally, the
+		fantasy genre doesn't pique my interest like other genres do. People
+		have already made alternatives on iOS and Android, but they don't
+		respect that the data belongs to the person using the app. I want to
+		access my data via API and export, and I don't want to be forced to
+		interact with an app directly in order to make good use of it.
+	</p>
+
+	<p>
+		This app may not satisfy everyone's tastes, but if you're looking for a
+		free Habitica alternative, this is a good option.
+	</p>
+
+	<p>
+		This app is independently developed and maintained by one person.
+		Profits, if any, go to paying his rent and caring for his 3 dogs.
+	</p>
+</div>
